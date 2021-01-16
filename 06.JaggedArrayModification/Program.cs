566ded8a779edf64fa1595ceb5684f8fc43f73c5
Teddy Aleksov <teddy57603@gmail.com>
2021-01-16 15:14:26 +0200

    added ex06. Jagged-Array Modification and ex 07. Pascal Triangle

---
+++ 06.JaggedArrayModification/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            matrix = MatrixInput(n);
+
+            string[] arr = Console.ReadLine().Split();
+
+            while (arr[0] != "END")
+            {
+                int row = int.Parse(arr[1]);
+                int col = int.Parse(arr[2]);
+
+                if (row >= 0 && col >= 0 && row < n && col < n)
+                {
+                    if (arr[0] == "Add")
+                    {
+                        matrix[int.Parse(arr[1]), int.Parse(arr[2])] += int.Parse(arr[3]);
+                    }
+                    else if (arr[0] == "Subtract")
+                    {
+                        matrix[int.Parse(arr[1]), int.Parse(arr[2])] -= int.Parse(arr[3]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+                arr = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static int[,]MatrixInput(int n)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int rows = 0; rows < n; rows++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int cols = 0; cols < n; cols++)
+                {
+                    matrix[rows, cols] = arr[cols];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ 07.PascalTriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[n][];
+
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                jaggedArray[i] = new long[i + 1];
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    if (j == 0 || j == jaggedArray[i].Length - 1)
+                    {
+                        jaggedArray[i][j] = 1;
+                    }
+                    else
+                    {
+                        jaggedArray[i][j] = jaggedArray[i - 1][j - 1] + jaggedArray[i - 1][j];
+                    }
+                }
+
+            }
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write($"{jaggedArray[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
