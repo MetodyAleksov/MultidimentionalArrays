0fe66b71feec023d473fd5d27945b6efe6294e5b
Teddy Aleksov <teddy57603@gmail.com>
2021-01-17 18:15:31 +0200

    updated

---
+++ 01.DiagonalDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int rows = 0; rows < n; rows++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int cols = 0; cols < n; cols++)
+                {
+                    matrix[rows, cols] = arr[cols];
+                }
+            }
+
+            int primarySum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                primarySum += matrix[i, i];
+            }
+
+            int secondarySum = 0;
+            int row = 0;
+            for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+            {
+                secondarySum += matrix[row, i];
+                row++;
+            }
+
+            Console.WriteLine(Math.Abs(primarySum - secondarySum));
+        }
+    }
+}
---
+++ 02.2x2SuqtaesinMatrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2x2SuqtaesinMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int equalMatrixes = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+
+                }
+            }
+        }
+    }
+}
