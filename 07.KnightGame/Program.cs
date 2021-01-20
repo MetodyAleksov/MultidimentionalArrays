d02368d9b53dbd70a0f8f0a162fb90983cc9be12
Teddy Aleksov <teddy57603@gmail.com>
2021-01-20 16:11:53 +0200

    updated

--- 07.KnightGame/Program.cs
+++ 07.KnightGame/Program.cs
@@ -12,94 +12,109 @@ namespace _07.KnightGame
 
             char[,] matrix = new char[n, n];
 
-            List<Knight> knights = new List<Knight>();
-
             for (int row = 0; row < n; row++)
             {
                 char[] arr = Console.ReadLine().ToCharArray();
                 for (int col = 0; col < n; col++)
                 {
-                    if (arr[col] == 'K')
-                    {
-                        knights.Add(new Knight(row, col));
-                    }
                     matrix[row, col] = arr[col];
                 }
             }
 
             int removedKnights = 0;
 
-            foreach (Knight knight in knights)
+            while(true)
             {
-                //row axis positive
-                if (isValid(knight.Row + 2, knight.Col - 1, n))
+                int maxAttackedKnights = 0;
+                int knightRow = 0;
+                int knightCol = 0;
+
+                for (int row = 0; row < n; row++)
                 {
-                    if (matrix[knight.Row + 2, knight.Col - 1] == 'K')
+                    for (int col = 0; col < n; col++)
                     {
-                        removedKnights++;
-                        matrix[knight.Row + 2, knight.Col - 1] = 'O';
+                        char symbow = matrix[row, col];
+
+                        if (symbow != 'K')
+                        {
+                            continue;
+                        }
+
+                        int count = GetCountofAttackedKnights(matrix, row, col);
+
+                        if (count > maxAttackedKnights)
+                        {
+                            maxAttackedKnights = count;
+                            knightRow = row;
+                            knightCol = col;
+                        }
                     }
                 }
-                if (isValid(knight.Row + 2, knight.Col + 1, n))
+
+                if (maxAttackedKnights == 0)
                 {
-                    if (matrix[knight.Row + 2, knight.Col + 1] == 'K')
-                    {
-                        removedKnights++;
-                        matrix[knight.Row + 2, knight.Col + 1] = 'O';
-                    }
+                    break;
                 }
-                //row axis negative
-                if (isValid(knight.Row - 2, knight.Col + 1, n))
-                {
-                    if (matrix[knight.Row - 2, knight.Col + 1] == 'K')
-                    {
-                        removedKnights++;
-                        matrix[knight.Row - 2, knight.Col + 1] = 'O';
-                    }
-                }
-                if (isValid(knight.Row - 2, knight.Col - 1, n))
-                {
-                    if (matrix[knight.Row - 2, knight.Col - 1] == 'K')
-                    {
-                        removedKnights++;
-                        matrix[knight.Row - 2, knight.Col - 1] = 'O';
-                    }
-                }
-                //col axies positive
-                if (isValid(knight.Row + 1, knight.Col + 2, n))
-                {
-                    if (matrix[knight.Row + 1, knight.Col + 2] == 'K')
-                    {
-                        removedKnights++;
-                        matrix[knight.Row + 1, knight.Col + 2] = 'O';
-                    }
-                }
-                if (isValid(knight.Row - 1, knight.Col + 2, n))
-                {
-                    if (matrix[knight.Row - 1, knight.Col + 2] == 'K')
-                    {
-                        removedKnights++;
-                        matrix[knight.Row - 1, knight.Col - 1] = 'O';
-                    }
-                }
-                //col axis negative
+
+                matrix[knightRow, knightCol] = '0';
+                removedKnights++;
             }
+
+            Console.WriteLine(removedKnights);
+        }
+
+        private static int GetCountofAttackedKnights(char[,] matrix, int row, int col)
+        {
+            int numOfAttackedKnights = 0;
+
+            if (isValid(row + 2, col - 1, matrix.GetLength(0)))
+            {
+                if (matrix[row + 2, col - 1] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            if (isValid(row + 2, col + 1, matrix.GetLength(0)))
+            {
+                if (matrix[row + 2, col + 1] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            if (isValid(row - 2, col - 1, matrix.GetLength(0)))
+            {
+                if (matrix[row - 2, col - 1] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            if (isValid(row - 2, col + 1, matrix.GetLength(0)))
+            {
+                if (matrix[row - 2, col + 1] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            if (isValid(row + 1, col + 2, matrix.GetLength(0)))
+            {
+                if (matrix[row + 1, col + 2] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            if (isValid(row + 1, col - 2, matrix.GetLength(0)))
+            {
+                if (matrix[row + 1, col - 2] == 'K')
+                {
+                    numOfAttackedKnights++;
+                }
+            }
+            return numOfAttackedKnights;
         }
 
         private static bool isValid(int row, int col, int n)
         {
             return row >= 0 && row < n && col >= 0 && col < n;
         }
-    }
-    class Knight
-    {
-        public Knight(int row, int col)
-        {
-            Row = row;
-            Col = col;
-        }
-
-        public int Row { get; set; }
-        public int Col { get; set; }
     }
 }
